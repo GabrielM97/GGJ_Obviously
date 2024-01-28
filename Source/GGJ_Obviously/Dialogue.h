19ee55ae0a4531b2b175f0d9765d6dd0f7d1b2df
gabriel <gabrielmenezes0297@gmail.com>
2024-01-28 10:43:19 +0000

    Dialogue DT

---
+++ Source/GGJ_Obviously/Dialogue.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "Dialogue.generated.h"
+class USoundCue;
+
+USTRUCT(BlueprintType)
+struct FDialogue :  public FTableRowBase
+{
+
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	TArray<FText> DialogueTexts;
+
+	UPROPERTY(EditAnywhere)
+	USoundCue* SpokenDialogue;
+};
